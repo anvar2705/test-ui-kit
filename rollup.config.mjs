3805c031b41a589c037f4fb9b7ae62aff9a844a2
Appazov <anvar978@yandex.ru>
2022-12-19 23:18:17 +0500

    Remove common js modules

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from './package.json' assert { type: 'json' }
 
 export default [
   {
@@ -10,11 +10,6 @@ export default [
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
         format: 'esm',
         sourcemap: true,
       },
@@ -22,7 +17,7 @@ export default [
     plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
