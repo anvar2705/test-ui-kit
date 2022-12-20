22e6e12c16d21859ca25ce8c225e70bf20ef8475
Appazov <anvar978@yandex.ru>
2022-12-20 09:50:21 +0500

    Try to add module css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { IButtonProps } from './Button.types'
-import './Button.css'
+import s from './Button.module.css'
 
 const Button = (props: IButtonProps): JSX.Element => {
   const { label, ...buttonProps } = props
-  return <button {...buttonProps}>{label}</button>
+  return (
+    <button {...buttonProps} className={s.root}>
+      {label}
+    </button>
+  )
 }
 
 export default Button
